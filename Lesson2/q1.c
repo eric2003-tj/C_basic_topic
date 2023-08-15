bf9002e616846c5a94e73d68cf00f3e7ac1c7e1b
Eric <102838561+eric2003-tj@users.noreply.github.com>
2023-08-15 10:20:51 +0800

    Create q1.c

---
+++ Lesson2/q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  int t;
+  scanf("%d",&t)
+  for(int i = 2;i*i<=t;i++){
+    if(t % i == 0){
+      printf("yes");
+      break;
+    }   
+  }
+  printf("no");
+  return 0;
+}
