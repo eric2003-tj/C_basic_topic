feb6b7822bb1229905460df3b2b6bf9ea71e04b7
Eric <102838561+eric2003-tj@users.noreply.github.com>
2023-08-21 15:15:37 +0800

    Create q1.c

---
+++ Lesson3/HW3_ans/q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int array[n];
+    int num = 0;
+    for(int i=0;i<n;i++){
+        scanf("%d",array+i);
+        if(array[i] == k){
+            num++;
+        }
+    }
+    printf("%d",num);
+    return 0;
+}
