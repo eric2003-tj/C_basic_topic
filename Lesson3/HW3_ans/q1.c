95590757e5ee05c50451040278051f13e6127702
Eric <102838561+eric2003-tj@users.noreply.github.com>
2023-08-22 12:56:47 +0800

    refactor the program

--- Lesson3/HW3_ans/q1.c
+++ Lesson3/HW3_ans/q1.c
@@ -5,7 +5,7 @@ int main(){
     int array[n];
     int num = 0;
     for(int i=0;i<n;i++){
-        scanf("%d",array+i);
+        scanf("%d",&array[i]);
         if(array[i] == k){
             num++;
         }
